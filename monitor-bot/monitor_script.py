a44c116ae7a417570906496642d06877b99b4999
FrancisDinh <tungdinh2821995@gmail.com>
2022-07-09 16:32:13 +0200

    get gpu

---
+++ monitor-bot/monitor_script.py
@@ -0,0 +1,22 @@
+def get_gpu_list():
+    pass
+
+
+def set_fan():
+    pass
+
+
+def set_ram():
+    pass
+
+
+def set_oc():
+    pass
+
+
+def send_message():
+    pass
+
+
+if __name__ == '__main__':
+    print()
